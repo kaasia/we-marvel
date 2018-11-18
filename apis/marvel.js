0afad9b6a1d64ae77e73c157479398979ef70a5f
zhuanghongji <zhuanghongjichina@gmail.com>
2018-11-18 14:40:57 +0800

    1. 网络请求支持缓存

--- apis/marvel.js
+++ apis/marvel.js
@@ -4,7 +4,7 @@ import {
 } from '../utils/index'
 
 import {
-  wxRequest,
+  wxRequestIfStorage,
 } from './wx'
 
 const BASE_URL = 'http://gateway.marvel.com/v1/public'
@@ -13,13 +13,22 @@ const BASE_URL_S = 'https://gateway.marvel.com/v1/public'
 const API_PUBLIC_KEY = '4dc572fd2a705860fabc560e57fa2ba9'
 const API_PRIVATE_KEY = '1599c9b56b2d6766c5466371ca404d784310ab69'
 
+/**
+ * 生成完整请求地址
+ * 
+ * @param {string} resourceName - 资源名称，"资源完整地址" 或 "资源地址后缀"
+ * @param {object} params - 额外参数
+ * @returns {string} 完整请求地址
+ */
 function genUrl(resourceName, params = {}) {
   const ts = new Date().getTime()
   const hashStr = `${ts}${API_PRIVATE_KEY}${API_PUBLIC_KEY}`
   const hashValue = getMD5(hashStr).toLocaleLowerCase()
 
-  // todo: init patams
-  const paramsStr = ''
+  let paramsStr = ''
+  for (const key in params) {
+    paramsStr += `&${key}=${params[key]}`
+  }
   let newResourceName = resourceName.replace(BASE_URL, '').replace(BASE_URL_S, '')
   const url = `${BASE_URL_S}${newResourceName}?${paramsStr}&ts=${ts}&apikey=${API_PUBLIC_KEY}&hash=${hashValue}`
   return url
@@ -36,13 +45,17 @@ function genUrl(resourceName, params = {}) {
  *   - 401 - Invalid Hash 
  *   - 405 - Method Not Allowed 
  *   - 403 - Forbidden 
+ * 
+ * @param {string} resourceName - 资源名称，"资源完整地址" 或 "资源地址后缀"
+ * @param {object} params - 额外参数
+ * @returns {Promise}
  */
 function marRquest(resourceName, params = {}) {
-  console.log('queryCharacters, params = ', params)
+  console.log(`marRquest: resourceName = ${resourceName}, params = `, params)
   const url = genUrl(resourceName, params)
 
   // res: the response of marvel api
-  return wxRequest(url).then(res => {
+  return wxRequestIfStorage(url).then(res => {
     if (res.code == 200) {
       return res.data
     }
--- apis/wx.js
+++ apis/wx.js
@@ -1,6 +1,9 @@
 
 /**
  * 封装了微信的网络请求
+ * 
+ * @param {string} url - 完整请求地址
+ * @returns {Promise}
  */
 function wxRequest(url) {
   console.log(`wx.request: url = ${url}`)
@@ -13,7 +16,7 @@ function wxRequest(url) {
           resolve(res.data)
           return
         }
-        reject(new Error(`statusCode = ${res.statusCode}`))
+        reject(new Error(`statusCode = ${res.statusCode}.`))
       },
       fail: (e) => {
         reject(e)
@@ -22,6 +25,88 @@ function wxRequest(url) {
   })
 }
 
+/**
+ * 获取微信缓存
+ * 
+ * @param {string} key - 本地缓存中指定的 key	
+ * @returns {Promise<object|string>} 
+ */
+function wxGetStorage(key) {
+  console.log(`wx.getStorage: key = ${key}`)
+  return new Promise((resolve, reject) => {
+    wx.getStorage({
+      key: key,
+      success: (res) => {
+        if (res.data) {
+          resolve(res.data)
+          return
+        }
+        reject(new Error(`no storage for key = ${key}.`))
+      },
+      fail: (e) => {
+        reject(e)
+      }
+    })
+  })
+}
+
+/**
+ * 设置微信缓存
+ * 
+ * @param {string} key - 本地缓存中指定的 key	
+ * @param {object} data - key对应的内容
+ * @returns {Promise<object|string>}
+ */
+function wxSetStorage(key, data) {
+  console.log(`wx.setStorage: key = ${key}, data = `, data)
+  return new Promise((resolve, reject) => {
+    wx.setStorage({
+      key: key,
+      data: data,
+      success: () => {
+        resolve(data)
+      },
+      fail: (e) => {
+        reject(e)
+      }
+    })
+  })
+}
+
+/**
+ * 发起微信请求(请求结果可能来自缓存)
+ * 
+ * - 有缓存的话就使用缓存
+ * - 没缓存的话请求成功后将数据进行缓存
+ * 
+ * @param {string} url - 完整请求地址
+ * @returns {Promise}
+ */
+function wxRequestIfStorage(url) {
+  console.log(`wxRequestIfStorage: url = ${url}`)
+  const key = url.substring(0, url.indexOf('&ts='))
+  return wxGetStorage(key).then(data => {
+    console.log(`有缓存：key = ${key}`)
+
+    return Promise.resolve(data)
+  }).catch(e => {
+    console.log(`没有缓存：key = ${key}, e = `, e)
+
+    return wxRequest(url).then(data => {
+      console.log(`微信请求成功：url = ${url}`)
+      wxSetStorage(key, data).then(data => {
+        console.log(`设置缓存成功：key = ${key}`)
+      }).catch(e => {
+        console.log(`设置缓存失败：key = ${key}, e = `, e)
+      })
+      return Promise.resolve(data)
+    })   
+  })
+}
+
 export {
   wxRequest,
+  wxGetStorage,
+  wxSetStorage,
+  wxRequestIfStorage,
 }
