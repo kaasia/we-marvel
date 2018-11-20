756a48f6e5d795b5fe921f9199c5a82643475a41
zhuanghongji <zhuanghongjichina@gmail.com>
2018-11-20 21:53:55 +0800

    1. 漫画详情增加 urls 按钮

--- pages/comic/comic.js
+++ pages/comic/comic.js
@@ -17,6 +17,33 @@ Page({
   },
 
   /**
+   * 点击了 comic.urls 中的链接
+   */
+  onUrlClick(e) {
+    console.log('onUrlClick, e = ', e)
+    const url = e.currentTarget.dataset.url 
+    wx.setClipboardData({
+      data: url,
+      success: () => {
+        wx.showToast({
+          title: '复制链接成功',
+          icon: 'success',
+          duration: 2000,
+        })
+      }
+    })
+  },
+
+  /**
+   * 点击了 resourceList 中的某个项
+   */
+  onResourceItemClick(e) {
+    console.log('onResourceItemClick, e = ', e)
+    const resourceURL = e.currentTarget.dataset.resourceuri 
+    console.log('onResourceItemClick, resourceURL = ', resourceURL)
+  },
+
+  /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
